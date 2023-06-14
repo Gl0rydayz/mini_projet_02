80a52f58b8aa6377ae436f0edbb59a3f2e250a8a
Oussama IOUIRY <oussamaiouiry@gmail.com>
2023-06-14 17:59:41 +0100

    Get Random Quote using JsonObjectRequest

--- app/src/main/java/com/example/mini_projet_02/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_02/MainActivity.java
@@ -10,6 +10,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -33,31 +34,24 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://dummyjson.com/quotes/random";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            tv_startActivityQuote.setText(jsonObject.getString("quote"));
-                            tv_startActivityAuthor.setText(jsonObject.getString("author"));
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }, new Response.ErrorListener() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    tv_startActivityQuote.setText(response.getString("quote"));
+                    tv_startActivityAuthor.setText(response.getString("author"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                tv_startActivityQuote.setText("That didn't work!");
+
             }
         });
-
-        // Set the tag on the request.
-        stringRequest.setTag("TAG");
-
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
 
         btn_startActivityPass.setOnClickListener(v -> {
             finish();
