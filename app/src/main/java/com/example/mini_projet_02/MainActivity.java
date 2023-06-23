10d510ffb01d39c95b9f193485f7575b1bfcd82d
Oussama IOUIRY <oussamaiouiry@gmail.com>
2023-06-23 09:20:10 +0100

    Work on Exercice 06 | V1 update

--- app/src/main/java/com/example/mini_projet_02/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_02/MainActivity.java
@@ -154,11 +154,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         String[] colorCodes = getResources().getStringArray(R.array.color_codes);
-        String[] colorNames = getResources().getStringArray(R.array.color_names);
         int itemId = item.getItemId();
         if (itemId >= 0 && itemId < colorCodes.length) {
-            String selectedColorCode = colorCodes[itemId];
-            getWindow().getDecorView().setBackgroundColor(Color.parseColor(selectedColorCode));
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor(colorCodes[itemId]));
         }
         return true;
     }
